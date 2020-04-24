ae7073c526366c9a00e56ac1d595e7967bb78860
Stanislav Petrakov <stas280501@yandex.ru>
2020-04-25 00:02:44 +0300

    First lesson

---
+++ Exercise2/Assets/Scripts/PrintFavoriteGames.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintFavoriteGames : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("R6S\n");
+        print("KC:D\tPoE\n\n");
+    }
+
+}
---
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    /// <summary>
+    /// First Console App Lecture code
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Prints a message
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            //print supportive message
+            Console.WriteLine("Hehe, I wrote C# code!");
+            Console.WriteLine();
+        }
+    }
+}
